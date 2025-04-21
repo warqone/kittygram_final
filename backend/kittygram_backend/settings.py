1a8cb71212cbc567f68888ef803e119e9a26aff2
Vadim Karimov <warqone12@yandex.ru>
2025-04-21 23:20:43 +0500

    Fix django secret key

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 load_dotenv(os.path.join(BASE_DIR, '.env'))
 
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 DEBUG = False
 
